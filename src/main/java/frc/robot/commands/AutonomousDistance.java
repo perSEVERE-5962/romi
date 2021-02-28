ed78a92631c58db2d9c4d115039203785a895f9a
david-lemasurier <david.lemasurier@gmail.com>
2021-02-27 23:04:14 -0500

    Darsh's code for Mondrian Madness submission

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -16,9 +16,31 @@ public class AutonomousDistance extends SequentialCommandGroup {
    */
   public AutonomousDistance(Drivetrain drivetrain) {
     addCommands(
-        new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(-0.5, 180, drivetrain),
-        new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(0.5, 180, drivetrain));
+        //start
+     new DriveDistance(0.6, 16.5, drivetrain), // drive forward at 60% speed for 16.5 inches
+     new TurnDegrees(-0.3, 80, drivetrain),  // turn 80 degrees to the left at 30% speed
+     new DriveDistance(0.6, 16.75, drivetrain), // drive forward at 60% speed for 16.75 inches
+ 
+     // at yellow
+     new TurnDegrees(-0.3, 87, drivetrain), // turn 87 degrees to the left at 30% speed
+ 
+     // enter center
+     new DriveDistance(0.5, 7, drivetrain), // drive forward at 50% speed for 7 inches
+     new TurnDegrees(-0.3, 30, drivetrain), // turn 30 degrees to the left at 30% speed
+     new DriveDistance(0.5, 9, drivetrain), // drive forward at 50% speed for 9 inches
+     new TurnDegrees(0.3, 60, drivetrain), // turn 60 degrees to the right at 30% speed
+     new DriveDistance(0.5, 9.75, drivetrain), // drive forward at 50% speed for 9.75 inches
+ 
+     // in blue
+     new TurnDegrees(0.3, 90, drivetrain), // turn 90 degrees to the right at 30% speed
+     new DriveDistance(0.6, 16.5, drivetrain), // drive forward at 60% speed for 16.5 inches
+ 
+     // in corner
+     new TurnDegrees(0.3, 90, drivetrain), // turn 90 degrees to the right at 30% speed
+     new DriveDistance(0.6, 15.5, drivetrain)); // drive forward at 60% speed for 15.5 inches
+ 
+     // done - in red
+
+    
   }
 }
